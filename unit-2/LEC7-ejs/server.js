47a2a73cd9fb566f6c3317023b1b6b45d10c5cda
ryandeist <ryandeist@gmail.com>
2024-11-04 17:03:53 -0500

    HTML Forms and EJS lecture notes added

--- unit-2/LEC7-ejs/server.js
+++ unit-2/LEC7-ejs/server.js
@@ -3,32 +3,36 @@
 const express = require('express');
 const app = express();
 
+// add the following:
 const inventory = [
-    { name: 'Candle', qty: 4 },
-    { name: 'Cheese', qty: 10 },
-    { name: 'Phone', qty: 1 },
-    { name: 'Tent', qty: 0 },
-    { name: 'Torch', qty: 5 }
-];
+  { name: 'Candle', qty: 4 },
+  { name: 'Cheese', qty: 10 },
+  { name: 'Phone', qty: 1 },
+  { name: 'Tent', qty: 0 },
+  { name: 'Torch', qty: 5 }
+]
 
 app.get('/', (req, res) => {
-    res.render('home.ejs', {
-        msg: 'Welcome to the Home Page',
-        player: {
-            name: 'Hunter'
-        },
-        inventory: inventory,
-    });
+  res.render('home.ejs', { 
+    msg: 'Here is your inventory',
+    player: {
+      name: "Ryan"
+    },
+    // change the following line to: 
+    inventory: inventory,
+  });
 });
 
-app.get("/:itemId", (req, res) => {
-    const index = req.params.itemId
+app.get('/:itemId', (req, res) => {
+    const index = req.params.itemId;
+    // render show.ejs, and pass it a single item object 
     res.render('show.ejs', {
-        item: inventory[index]
+      item: inventory[index]
     });
-})
+  });
+  
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
-
+  console.log('Listening on port 3000');
 });
+
