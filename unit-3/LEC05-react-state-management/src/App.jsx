f4cc896117af05aac8cca22659afd5d4de1434fe
ryandeist <ryandeist@gmail.com>
2024-12-09 19:24:23 -0500

    React State Management - Code Along Complete

--- unit-3/LEC05-react-state-management/src/App.jsx
+++ unit-3/LEC05-react-state-management/src/App.jsx
@@ -1,10 +1,36 @@
 // src/App.jsx
-
+import { useState } from 'react'
 import './App.css';
 
 const App = () => {
+  const [mode, setMode] = useState('light');
+
+  const [userObj, setUserObj] = useState({
+    firstName: 'Ryan',
+    lastName: 'Deist',
+    hasPets: false,
+    age: 31,
+  });
+
+  // console.log(userObj);
+
+  const handleMode = (modeValue) => {
+    console.log(modeValue);
+    setMode(modeValue);
+  };
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <div className={mode}>
+        <h1>{`Hi my name is ${userObj.firstName} ${userObj.lastName}. I am ${userObj.age} and ${userObj.hasPets ? 'do have' : `don't have`} pets.`}</h1>
+      </div>
+      <div>
+        <button onClick={() => handleMode('dark')}>Dark Mode</button>
+        <button onClick={() => handleMode('light')}>Light Mode</button>
+        <button onClick={() => handleMode('neon')}>Neon Mode</button>
+        <button onClick={() => handleMode('night')}>Night Mode</button>
+      </div>
+    </>
   );
 };
 
