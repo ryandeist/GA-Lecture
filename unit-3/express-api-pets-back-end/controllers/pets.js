071b3cb574698b7f0d676fe7eee25eba60de05a1
Ryan Deist <ryandeist@gmail.com>
2025-01-09 19:30:29 -0500

    delete, update routes added and tested

--- unit-3/express-api-pets-back-end/controllers/pets.js
+++ unit-3/express-api-pets-back-end/controllers/pets.js
@@ -26,13 +26,55 @@ router.get('/:petId', async (req, res) => {
     try {
         const foundPet = await Pet.findById(req.params.petId);
         if (!foundPet) {
-            res.status(404).json({ error: 'Pet not found' });
+            res.status(404);
+            throw new Error('The pet was not found');
         };
         res.status(200).json(foundPet);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        if (res.statusCode === 404) {
+            res.json({ error: 'The pet was not found' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        };
+    };
+});
+
+router.delete('/:petId', async (req, res) => {
+    try {
+        const deletedPet = await Pet.findByIdAndDelete(req.params.petId);
+        if (!deletedPet) {
+            res.status(404);
+            throw new Error('The pet was not found');
+        };
+        res.status(200).json(deletedPet);
+    } catch (error) {
+        console.log(error);
+        if (res.statusCode === 404) {
+            res.json({ error: 'The pet was not found' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        };
     }
-})
+});
+
+router.put('/:petId', async (req, res) => {
+    try {
+        const updatedPet = await Pet.findByIdAndUpdate(req.params.petId, req.body, { new: true });
+        if (!updatedPet) {
+            res.status(404);
+            throw new Error('The pet was not found');
+        };
+        res.status(200).json(updatedPet);
+    } catch (error) {
+        console.log(error);
+        if (res.statusCode === 404) {
+            res.json({ error: 'The pet was not found' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        };
+    };
+    // res.json({ message: `PUT /pets/${req.params.petId}` });
+});
 
 module.exports = router;
