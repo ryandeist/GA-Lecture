a2502c5f2be34d6658a7c1e101571091698c869a
Ryan Deist <ryandeist@gmail.com>
2025-01-19 17:57:19 -0500

    Sign Up Form Component made

---
+++ unit-3/LEC14-react-jwt-auth-template/src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+const SignUpForm = () => {
+    const navigate = useNavigate();
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        passwordConf: '',
+    });
+
+    const { username, password, passwordConf } = formData;
+
+    const handleChange = (event) => {
+        setMessage('');
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    };
+
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id='name'
+                        value={username}
+                        name='username'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        value={password}
+                        name='password'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <button onClick={() => navigate('/')}>Cancel</button>
+                </div>
+            </form>
+        </main>
+    )
+};
+
+export default SignUpForm;
